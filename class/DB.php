7f0ba5f1a5c155cb6c06a921a88bd8ccfd9bccde
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-19 08:37:34 -0300

    upload

--- class/DB.php
+++ class/DB.php
@@ -9,9 +9,15 @@ class DB{
 self::$pdo = new PDO('mysql:host='.SERVIDOR.';dbname='.BANCO,USUARIO,SENHA);
 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPETION);
 self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-            }catch() {
-
+            }catch(PDOException $erro) {
+             echo "Falha ao se conectar com o banco: ".$erro->getMessge();
             }
         }
+
+        return self::$pdo;
+    }
+
+    public static function prepare($sql) {
+        return self::instanciar()->prepare($sql);
     }
 }
